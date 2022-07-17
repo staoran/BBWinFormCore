798d6a67dc7f4eac752389b2101fe64cc87a5136
taoran <staoran@gmail.com>
2022-07-17 22:25:41 +0800

    增加 GlobalUsings.cs

---
+++ src/BB.Core/GlobalUsings.cs
@@ -0,0 +1,17 @@
+﻿global using Furion;
+global using Furion.DataEncryption;
+global using Furion.DataValidation;
+global using Furion.DependencyInjection;
+global using Furion.DynamicApiController;
+global using Furion.Extensions;
+global using Furion.FriendlyException;
+global using Mapster;
+global using Microsoft.AspNetCore.Authorization;
+global using Microsoft.AspNetCore.Http;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.CodeAnalysis;
+global using System.ComponentModel.DataAnnotations;
+global using Furion.JsonSerialization;
+global using Furion.LinqBuilder;
+global using Furion.Logging.Extensions;
+global using SqlSugar;
