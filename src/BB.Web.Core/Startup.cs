d370ce8566dfdadbaaa8e803616598dc9ca6bb01
taoran <staoran@gmail.com>
2022-07-18 15:42:06 +0800

    增加 JWT 动态API 多语言 真实IP JSON 友好异常 配置

--- src/BB.Web.Core/Startup.cs
+++ src/BB.Web.Core/Startup.cs
@@ -1,6 +1,9 @@
-﻿using Furion;
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using Furion;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Serilog;
@@ -11,20 +14,41 @@ public class Startup : AppStartup
 {
     public void ConfigureServices(IServiceCollection services)
     {
-        services.AddJwt<JwtHandler>();
+        // 启用jwt认证, 并启用全局授权验证
+        services.AddJwt<JwtHandler>(enableGlobalAuthorize: true);
 
+        // 跨域
         services.AddCorsAccessor();
 
         services.AddControllers()
+            .AddAppLocalization() // 注册多语言
+            .AddJsonOptions(options =>
+            {
+                // 驼峰命名
+                options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+                // 时间格式化
+                options.JsonSerializerOptions.Converters.AddDateFormatString("yyyy-MM-dd HH:mm:ss");
+                // 忽略循环引用
+                options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+            })
             .AddInjectWithUnifyResult();
     }
 
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
-        if (env.IsDevelopment())
-        {
+        if (env.IsDevelopment()) 
             app.UseDeveloperExceptionPage();
-        }
+
+        // NGINX 反向代理获取真实IP
+        app.UseForwardedHeaders(new ForwardedHeadersOptions
+        {
+            ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto,
+            KnownNetworks = {  },
+            KnownProxies = {  }
+        });
+
+        // 配置多语言，必须在 路由注册之前
+        app.UseAppLocalization();
 
         app.UseHttpsRedirection();
 
