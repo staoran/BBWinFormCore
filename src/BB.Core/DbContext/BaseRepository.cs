5001e19777b09ef24f4d22ff93cd89f5827840da
taoran <staoran@gmail.com>
2022-07-18 22:47:52 +0800

    初始化仓储基类

---
+++ src/BB.Core/DbContext/BaseRepository.cs
@@ -0,0 +1,15 @@
+﻿using BB.Entity.Base;
+
+namespace BB.Core.DbContext;
+
+/// <summary>
+/// 仓储基类
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public class BaseRepository<T> : SimpleClient<T> where T : BaseEntity, new()
+{
+    public BaseRepository(IUnitOfWork unitOfWork) : base(unitOfWork.GetDbClient())
+    {
+        Context = unitOfWork.GetDbClient();
+    }
+}
