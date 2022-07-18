6875b43f60578be9ee072827f79caa61ed413c3e
taoran <staoran@gmail.com>
2022-07-18 22:46:47 +0800

    新增 SqlSugar 事务工作单元

---
+++ src/BB.Core/DbContext/IUnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿namespace BB.Core.DbContext;
+
+/// <summary>
+/// 单例的 SqlSugar 工作单元
+/// </summary>
+public interface IUnitOfWork
+{
+    /// <summary>
+    /// 获取单例 SqlSugarScope 实例
+    /// </summary>
+    /// <returns></returns>
+    SqlSugarScope GetDbClient();
+
+    /// <summary>
+    /// 开启事务
+    /// </summary>
+    void BeginTran();
+
+    /// <summary>
+    /// 提交事务
+    /// </summary>
+    void CommitTran();
+
+    /// <summary>
+    /// 回滚事务
+    /// </summary>
+    void RollbackTran();
+}
---
+++ src/BB.Core/DbContext/UnitOfWork.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace BB.Core.DbContext;
+
+/// <summary>
+/// 单例的 SqlSugar 工作单元
+/// </summary>
+public class UnitOfWork : IUnitOfWork
+{
+    /// <summary>
+    /// 单例的 SqlSugar 实例
+    /// </summary>
+    private readonly ISqlSugarClient _sqlSugarClient;
+
+    /// <summary>
+    /// 事务计数
+    /// </summary>
+    private int TranCount { get; set; }
+
+    /// <summary>
+    /// 构造函数
+    /// </summary>
+    /// <param name="sqlSugarClient">SqlSugar 实例</param>
+    public UnitOfWork(ISqlSugarClient sqlSugarClient)
+    {
+        _sqlSugarClient = sqlSugarClient;
+        TranCount = 0;
+    }
+
+    /// <summary>
+    /// 获取单例 SqlSugarScope 实例
+    /// </summary>
+    /// <returns></returns>
+    public SqlSugarScope GetDbClient() => (SqlSugarScope)_sqlSugarClient;
+
+    /// <summary>
+    /// 开启事务
+    /// </summary>
+    public void BeginTran()
+    {
+        lock (this)
+        {
+            TranCount++;
+            GetDbClient().BeginTran();
+        }
+    }
+
+    /// <summary>
+    /// 提交事务
+    /// </summary>
+    public void CommitTran()
+    {
+        lock (this)
+        {
+            TranCount--;
+            if (TranCount == 0)
+            {
+                try
+                {
+                    GetDbClient().CommitTran();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    GetDbClient().RollbackTran();
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// 回滚事务
+    /// </summary>
+    public void RollbackTran()
+    {
+        lock (this)
+        {
+            TranCount--;
+            GetDbClient().RollbackTran();
+        }
+    }
+}
