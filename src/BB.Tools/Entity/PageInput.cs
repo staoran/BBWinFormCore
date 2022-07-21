ff9ab6286513c3fde34fa688c3c185d108d518e1
taoran <staoran@gmail.com>
2022-07-21 15:32:02 +0800

    新增 分页入参 和 分页返回格式类

---
+++ src/BB.Tools/Entity/PageInput.cs
@@ -0,0 +1,27 @@
+﻿namespace BB.Tools.Entity;
+
+/// <summary>
+/// 通用分页入参
+/// </summary>
+public class PageInput
+{
+    /// <summary>
+    /// 当前页码
+    /// </summary>
+    public virtual int PageNo { get; set; } = 1;
+
+    /// <summary>
+    /// 页码容量
+    /// </summary>
+    public virtual int PageSize { get; set; } = 20;
+
+    /// <summary>
+    /// 排序字段
+    /// </summary>
+    public virtual string SortField { get; set; }
+
+    /// <summary>
+    /// 排序方法,默认升序,否则降序
+    /// </summary>
+    public virtual string SortOrder { get; set; } = "Desc";
+}
---
+++ src/BB.Tools/Entity/PageResult.cs
@@ -0,0 +1,42 @@
+﻿namespace BB.Tools.Entity;
+
+/// <summary>
+/// 统一的分页格式
+/// </summary>
+public class PageResult<T> where T : new()
+{
+    /// <summary>
+    /// 页码
+    /// </summary>
+    public int PageNo { get; set; }
+    
+    /// <summary>
+    /// 页条数
+    /// </summary>
+    public int PageSize { get; set; }
+    
+    /// <summary>
+    /// 总页数
+    /// </summary>
+    public int TotalPage { get; set; }
+    
+    /// <summary>
+    /// 总条数
+    /// </summary>
+    public int TotalRows { get; set; }
+    
+    /// <summary>
+    /// 当前页集合
+    /// </summary>
+    public IEnumerable<T> Rows { get; set; }
+
+    /// <summary>
+    /// 是否有上一页
+    /// </summary>
+    public bool HasPrevPages => PageNo > 1;
+
+    /// <summary>
+    /// 是否有下一页
+    /// </summary>
+    public bool HasNextPages => PageNo < TotalPage;
+}
