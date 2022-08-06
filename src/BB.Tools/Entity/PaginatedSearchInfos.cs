3dccfa7fb9c8b6f4266307c7bd604da18439e1a7
taoran <staoran@gmail.com>
2022-08-06 18:09:41 +0800

    增加分页动态搜索条件类

---
+++ src/BB.Tools/Entity/PaginatedSearchInfos.cs
@@ -0,0 +1,8 @@
+﻿namespace BB.Tools.Entity;
+
+/// <summary>
+/// 分页搜索条件
+/// </summary>
+/// <param name="SearchInfos">搜索条件</param>
+/// <param name="PagerInfo">分页条件</param>
+public record PaginatedSearchInfos(SearchInfo[] SearchInfos, PageInput PagerInfo);
