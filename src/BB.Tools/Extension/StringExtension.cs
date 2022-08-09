f09caabb75db4d14a8c490aa0067012fd3b00c3a
taoran <staoran@gmail.com>
2022-08-09 21:39:59 +0800

    增加一些字符串操作扩展

--- src/BB.Tools/Extension/StringExtension.cs
+++ src/BB.Tools/Extension/StringExtension.cs
@@ -1,5 +1,7 @@
-﻿using System.Globalization;
+﻿using System.Collections;
+using System.Globalization;
 using System.Net;
+using System.Reflection;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Security.Cryptography;
@@ -150,6 +152,182 @@ public static class StringExtension
         }
 
         return Path.Combine(val, path2);
+    }
+
+    /// <summary>
+    /// 转换为Camel字符串格式，去掉字符之间的空格以及起始"_"符号
+    /// </summary>
+    /// <param name="name">待转换字符串</param>
+    /// <returns></returns>
+    public static string ToCamel(string name)
+    {
+        string clone = name.TrimStart('_');
+        clone = RemoveSpaces(ToProperCase(clone));
+        return $"{Char.ToLower(clone[0])}{clone.Substring(1, clone.Length - 1)}";
+    }
+
+    /// <summary>
+    /// 转换为Capital格式显示，去掉字符之间的空格以及起始"_"符号
+    /// </summary>
+    /// <param name="name">待转换字符串</param>
+    /// <returns></returns>
+    public static string ToCapit(String name)
+    {
+        string clone = name.TrimStart('_');
+        return RemoveSpaces(ToProperCase(clone));
+    }
+
+    /// <summary>
+    /// 移除字符串最后的一个字符
+    /// </summary>
+    /// <param name="s"></param>
+    /// <returns></returns>
+    public static string RemoveFinalChar(string s)
+    {
+        if (s.Length > 1)
+        {
+            s = s.Substring(0, s.Length - 1);
+        }
+        return s;
+    }
+
+    /// <summary>
+    /// 移除字符串中最后的一个逗号
+    /// </summary>
+    /// <param name="s">操作的字符串</param>
+    /// <returns></returns>
+    public static string RemoveFinalComma(string s)
+    {
+        if (s.Trim().Length > 0)
+        {
+            int c = s.LastIndexOf(",");
+            if (c > 0)
+            {
+                s = s.Substring(0, s.Length - (s.Length - c));
+            }
+        }
+        return s;
+    }
+
+    /// <summary>
+    /// 清除字符间的空格，并转换为合适的大小写
+    /// </summary>
+    /// <param name="s"></param>
+    /// <returns></returns>
+    public static string ToTrimmedProperCase(this string s)
+    {
+        return RemoveSpaces(ToProperCase(s));
+    }
+
+    /// <summary>
+    /// 在字符串中，指定开始字符和结束字符，提取中间的内容
+    /// </summary>
+    /// <param name="content">待操作字符串</param>
+    /// <param name="start">开始字符</param>
+    /// <param name="end">结束字符</param>
+    /// <returns></returns>
+    public static ArrayList ExtractInnerContent(this string content, string start, string end)
+    {
+        int sindex = -1, eindex = -1;
+        int msindex = -1, meindex = -1;
+        int span = 0;
+
+        ArrayList al = new ArrayList();
+
+        sindex = content.IndexOf(start);
+        msindex = sindex + start.Length;
+        eindex = content.IndexOf(end, msindex);
+        span = eindex - msindex;
+
+        if (sindex >= 0 && eindex > sindex)
+        {
+            al.Add(content.Substring(msindex, span));
+        }
+
+        while (sindex >= 0 && eindex > 0)
+        {
+            sindex = content.IndexOf(start, eindex);
+            if (sindex > 0)
+            {
+                eindex = content.IndexOf(end, sindex);
+                msindex = sindex + start.Length;
+                span = eindex - msindex;
+
+                if (msindex > 0 && eindex > 0)
+                {
+                    al.Add(content.Substring(msindex, span));
+                }
+            }
+        }
+
+        return al;
+    }
+
+    /// <summary>
+    /// 在字符串中，指定开始字符和结束字符，提取非中间的数据
+    /// </summary>
+    /// <param name="content">待操作的字符</param>
+    /// <param name="start">开始字符</param>
+    /// <param name="end">结束字符</param>
+    /// <returns></returns>
+    public static ArrayList ExtractOuterContent(this string content, string start, string end)
+    {
+        int sindex = -1, eindex = -1;
+
+        ArrayList al = new ArrayList();
+
+        sindex = content.IndexOf(start);
+        eindex = content.IndexOf(end);
+        if (sindex >= 0 && eindex > sindex)
+        {
+            al.Add(content.Substring(sindex, eindex + end.Length - sindex));
+        }
+
+        while (sindex >= 0 && eindex > 0)
+        {
+            sindex = content.IndexOf(start, eindex);
+            if (sindex > 0)
+            {
+                eindex = content.IndexOf(end, sindex);
+                if (sindex > 0 && eindex > 0)
+                {
+                    al.Add(content.Substring(sindex, eindex + end.Length - sindex));
+                }
+            }
+        }
+
+        return al;
+    }
+
+    /// <summary>
+    /// 去除指定字符串前缀的算法
+    /// </summary>
+    /// <param name="content">待除去特定字符串的内容</param>
+    /// <param name="prefixString">特定字符串列表(以逗号,分号,空格等标识)</param>
+    /// <returns></returns>
+    public static string RemovePrefix(this string content, string prefixString)
+    {
+        if (string.IsNullOrEmpty(prefixString) || prefixString.Trim() == string.Empty)
+        {
+            return content;
+        }
+
+        char[] splitChars = new[] {',', ';', ' '};            
+        string strReturn = content;
+        prefixString = prefixString.Trim(splitChars); //过滤前后多余的分隔符号,否则容易出错
+
+        string[] suffixArray = prefixString.Split(splitChars);
+        foreach (string suffix in suffixArray)
+        {
+            int sindex = strReturn.IndexOf(suffix, StringComparison.OrdinalIgnoreCase);// 非大小写敏感
+            if (sindex == 0)
+            {
+                strReturn = strReturn.Substring(suffix.Length);
+                break; //匹配一次就应该出来
+            }
+        }
+
+        return strReturn;
     }
 
     #endregion
@@ -331,10 +509,9 @@ public static class StringExtension
     /// int.Parse("1 000 000".RemoveSpaces(),.....
     /// </summary>
     /// <param name="s"></param>
-    /// <param name="value">没有空白的字符串</param>
     public static string RemoveSpaces(this string s)
     {
-        return s.Replace(" ", "");
+        return s.Trim().Replace(" ", "");
     }
 
     /// <summary>
