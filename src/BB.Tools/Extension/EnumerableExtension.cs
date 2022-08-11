3b8bb741440eab06afc1e5db0d931efb9c83cd39
taoran <staoran@gmail.com>
2022-08-11 22:53:42 +0800

    新增异步 ForEach 扩展

--- src/BB.Tools/Extension/EnumerableExtension.cs
+++ src/BB.Tools/Extension/EnumerableExtension.cs
@@ -71,6 +71,27 @@ public static class EnumerableExtension
     {
         ForEach(val, (o, i) => action(o));
     }
+    
+    public static Task ForEachAsync<TSource, TResult>(
+        this IEnumerable<TSource> source,
+        Func<TSource, Task<TResult>> taskSelector, Action<TSource, TResult> resultProcessor)
+    {
+        var oneAtATime = new SemaphoreSlim(initialCount:1, maxCount:1);
+        return Task.WhenAll(
+            from item in source
+            select ProcessAsync(item, taskSelector, resultProcessor, oneAtATime));
+    }
+
+    private static async Task ProcessAsync<TSource, TResult>(
+        TSource item,
+        Func<TSource, Task<TResult>> taskSelector, Action<TSource, TResult> resultProcessor,
+        SemaphoreSlim oneAtATime)
+    {
+        TResult result = await taskSelector(item);
+        await oneAtATime.WaitAsync();
+        try { resultProcessor(item, result); }
+        finally { oneAtATime.Release(); }
+    }
 
     /// <summary>
     /// Alternates the specified first.
