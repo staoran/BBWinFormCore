0b9dcf63eb1182ff4346ba8036e18811ec5c8a19
taoran <staoran@gmail.com>
2022-08-30 16:16:33 +0800

    增加 RESTfulResultControl 类，增加 Handling 方法用于判断返回结果是否成功。

---
+++ src/BB.HttpServices/Base/RESTfulResultHandling.cs
@@ -0,0 +1,18 @@
+﻿using Furion.FriendlyException;
+using Furion.JsonSerialization;
+using Furion.UnifyResult;
+
+namespace BB.HttpServices.Base;
+
+public class RESTfulResultControl<T> : RESTfulResult<T>
+{
+    public T Handling()
+    {
+        if (Succeeded)
+        {
+            return Data;
+        }
+        
+        throw Oops.Bah(JSON.Serialize(Errors), Data, Extras, Errors);
+    }
+}
